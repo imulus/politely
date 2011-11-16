2bffe5be8dd7b266cbe0aa3d39e51dc0280cbd01
Casey O'Hara <caseyo@caseyohara.com>
2011-11-15 17:40:07 -0700

    Added JSON actions, minimal CSS

--- app.rb
+++ app.rb
@@ -4,9 +4,21 @@ Bundler.require
 require './politely'
 
 class App < Sinatra::Base
+ 	set :public_folder, File.dirname(__FILE__) + '/public'
 	
 	get "/" do
-		Politely.random
+		@compliment = Politely.random
+    erb :index		
 	end
+	
+	get "/compliments.json" do
+		content_type :json
+		Politely.compliments.to_json
+	end	
+	
+	get "/random.json" do
+		content_type :json
+		Politely.random.to_json
+	end	
 
 end
